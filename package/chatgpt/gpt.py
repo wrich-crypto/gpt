5d7fac3e18bd84ae58c842f327a9b74aa9a5c1c1
yeye1995 <416116073@qq.com>
2023-04-10 15:29:33 +0800

    debug

--- package/chatgpt/gpt.py
+++ package/chatgpt/gpt.py
@@ -7,7 +7,7 @@ def gpt_content(prompt, max_try=1):
     current_try = 0
 
     while current_try < max_try:
-        proxy = hot_config.get_next_proxy()
+        # proxy = hot_config.get_next_proxy()
         api_key = hot_config.get_next_api_key()
 
         try:
@@ -15,15 +15,15 @@ def gpt_content(prompt, max_try=1):
 
             session = requests.Session()
 
-            if proxy is not None and proxy != '':
-                session.proxies = {'https': proxy}
+            # if proxy is not None and proxy != '':
+            #     session.proxies = {'https': proxy}
 
             response = openai.Completion.create(
                 model="text-davinci-003",
                 prompt=prompt,
                 temperature=0.5,
                 max_tokens=2048,
-                session=session
+                # session=session
             )
             current_try = current_try + 1
             return response.choices[0]
