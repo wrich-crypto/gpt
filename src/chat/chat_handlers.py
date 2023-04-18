794e393985bbf6e7124dc2769cc4670110f6bb04
cryptocrich <416116073@qq.com>
2023-04-18 17:18:16 +0800

    channel

--- src/chat/chat_handlers.py
+++ src/chat/chat_handlers.py
@@ -25,7 +25,7 @@ def create_stream_with_retry(message, channel=None, max_attempts=None):
         if create_stream_response and str(create_stream_response["code"]) == '0':
             logger.info(f'chat gpt response: {create_stream_response}')
             stream_id = create_stream_response["data"]["streamId"]
-            return chat_api.get_stream(stream_id)
+            return chat_api.get_stream(stream_id), channel_id
         elif str(create_stream_response["code"]) == '1':
             logger.error(f'chat gpt error: {create_stream_response}')
             hot_config.remove_api_key(access_token)
@@ -37,7 +37,7 @@ def create_stream_with_retry(message, channel=None, max_attempts=None):
 def generate(channel, message, token, messageId, tokens_consumed):
     try:
         channel = channel
-        stream_response = create_stream_with_retry(message, channel, 3)
+        stream_response, channel_id = create_stream_with_retry(message, channel, 3)
 
         content = ''
         for chunk in stream_response.iter_content(chunk_size=1024):
@@ -58,7 +58,6 @@ def generate(channel, message, token, messageId, tokens_consumed):
             logger.error(f'Invalid token, token:{token}')
             return
 
-        channel_id = 0
         if ChatChannel.exists(new_session, channel_id=channel_id, user_id=user.id, status=status_success) is False:
             ChatChannel.create(new_session, channel_id=channel_id, user_id=user.id, status=status_success, title=message)
 
