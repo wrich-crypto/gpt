4515aabffc7c4f5a6398bb9dc6905c83cf8aa273
cryptocrich <416116073@qq.com>
2023-04-24 21:27:10 +0800

    chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# from gevent import monkey
-# monkey.patch_all()
-# from gevent.pywsgi import WSGIServer
+from gevent import monkey
+monkey.patch_all()
+from gevent.pywsgi import WSGIServer
 from init import *
 from flask import Flask, request
 from src.user.user_handlers import user_bp
@@ -35,6 +35,6 @@ def teardown_request(exception):
 
 if __name__ == '__main__':
     print('server begin')
-    # WSGIServer((main_config.server, main_config.port), app).serve_forever()
-    app.run(host=main_config.server, port=main_config.port)
+    WSGIServer((main_config.server, main_config.port), app).serve_forever()
+    # app.run(host=main_config.server, port=main_config.port)
     print('server end')
